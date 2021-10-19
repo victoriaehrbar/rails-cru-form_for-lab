4d06fc7768a9bbf8e6eb876426407debcd7eb81f
github username <72711695+victoriaehrbar@users.noreply.github.com>
2021-10-18 19:46:51 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'songs/edit'
-
-  get 'songs/new'
-
-  get 'songs/show'
-
-  get 'genres/edit'
-
-  get 'genres/new'
-
-  get 'genres/show'
+  resources :songs
+  resources :genres
+  resources :artists
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
